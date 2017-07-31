e393f7fb2247e3ceb15f2b0dbd20468279724faa
chenhua <chenhua22@outlook.com>
2017-07-31 20:41:49 +0800

    添加json练习

--- struct.go
+++ struct.go
@@ -23,11 +23,21 @@ func main() {
 	}
 	fmt.Println(a.name, a.B.name)
 	fmt.Println(b)
-	// 尝试改变结构体之后, 得出结论：结构体传递的值
+	// 尝试改变结构体之后, 得出结论：结构体是值传递
 	changeStruct(b)
 	fmt.Println(b)
+	// 一般直接获取地址即可, 而且直接访问不需要反向取址
+	test := &A{name: "jiefu", B: B{age: 19}}
+	test.age = 1000
+	fmt.Println(test)
+	change(test)
+	fmt.Println(test)
 }
 
 func changeStruct(person B) {
 	person.name = "jiefu"
+}
+
+func change(test *A) {
+	test.name = "changed"
 }
